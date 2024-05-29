20adfad391d6a4d3177f6640be6500b7de9b17ae
Eduard4002 <54219870+Eduard4002@users.noreply.github.com>
2024-05-29 13:09:15 +0200

    Renamed timeline page

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,10 @@
+class navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
 
-
-class navbar extends HTMLElement{
-
-    constructor(){
-        super();
-    }
-
-    connectedCallback(){
-
-        this.innerHTML =
-             `
+  connectedCallback() {
+    this.innerHTML = `
         
         <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-white py-3">
         <div class="container px-5">
@@ -18,7 +13,7 @@ class navbar extends HTMLElement{
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
             <li class="nav-item"><a class="nav-link" href="index.html">Hem</a></li>
-            <li class="nav-item"><a class="nav-link" href="resume.html">Timeline</a></li>
+            <li class="nav-item"><a class="nav-link" href="timeline.html">Timeline</a></li>
             <li class="nav-item"><a class="nav-link" href="projects.html">Projekt</a></li>
             <li class="nav-item"><a class="nav-link" href="index.html#about">about</a></li>
             </ul>
@@ -26,8 +21,7 @@ class navbar extends HTMLElement{
         </div>
     </nav>
         `;
-
-    }
+  }
 }
 
-customElements.define('nav-bar', navbar);
+customElements.define("nav-bar", navbar);
